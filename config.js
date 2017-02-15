7b598c6628ac28708f5fa5483ecee9d4308fedae
Marvin Zwolsman <marvin.zwolsman@gmail.com>
2017-02-15 12:14:48 +0100

    Updated the HOST to lensert.com

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const HOST = "http://92.109.3.160";
+const HOST = "http://lensert.com";
 const PORT = 3000;
 
 module.exports = {
     url: HOST + ':' + PORT,
     apiUrl: HOST + ':' + PORT + '/api/',
-    dbUrl: '127.0.0.1:5984',
+    dbUrl: 'http://127.0.0.1:5984',
     dbName: 'test'
 };
